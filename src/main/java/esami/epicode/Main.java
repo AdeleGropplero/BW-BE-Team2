e566f412210effb9a9e664ac2985452e3a427f3d
Andrea Saccomanni <Andrea Saccomanni>
2025-01-28 11:29:26 +0100

    commit prova

--- src/main/java/esami/epicode/Main.java
+++ src/main/java/esami/epicode/Main.java
@@ -1,17 +1,7 @@
 package esami.epicode;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+public class Main {
+    public static void main(String[] args) {
 
-
-public class Main 
-{     public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("TrasportoPubblico_Team2");
-    public static EntityManager em = emf.createEntityManager();
-    public static void main( String[] args ) {
-
-        em.close();
-        emf.close();
-        //
     }
 }
